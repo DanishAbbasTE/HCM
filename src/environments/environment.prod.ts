af30e5d2c046862593bd8c1d2cf44f3b35504064
Saad Tahir <saadtahir2657@outlook.com>
2023-10-17 21:32:59 +0500

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL : "http://shahwaizhasan-001-site1.etempurl.com/api/"
+  // API_URL : "http://shahwaizhasan-001-site1.etempurl.com/api/"
+  
+  API_URL : "http://optiwise-001-site1.etempurl.com/api/"
 };
